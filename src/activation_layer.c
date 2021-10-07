b5d4ed83c6f7db670dbbcbbe8166fbdb05e603f3
Yuan <yuanw24@cs.washington.edu>
2021-10-06 20:10:05 -0700

    [lab0] finished activation layer

--- src/activation_layer.c
+++ src/activation_layer.c
@@ -3,6 +3,11 @@
 #include <assert.h>
 #include "uwnet.h"
 
+#define LRELU_COEFFICIENT 0.01
+
+float logistic(float x) {
+    return  1.0 / (1 + exp(-x));
+}
 
 // Run an activation layer on input
 // layer l: pointer to layer to run
@@ -24,6 +29,40 @@ matrix forward_activation_layer(layer l, matrix x)
     // relu(x)     = x if x > 0 else 0
     // lrelu(x)    = x if x > 0 else .01 * x
     // softmax(x)  = e^{x_i} / sum(e^{x_j}) for all x_j in the same row 
+    switch(l.activation) {
+        case LINEAR:
+            break;
+        case LOGISTIC:
+            for (int i = 0; i < y.rows * y.cols; i++) {
+                y.data[i] = logistic(y.data[i]);
+            }
+            break;
+        case RELU:
+            for (int i = 0; i < y.rows * y.cols; i++) {
+                if (y.data[i] < 0) {
+                    y.data[i] = 0;
+                }
+            }
+            break;
+        case LRELU:
+            for (int i = 0; i < y.rows * y.cols; i++) {
+                if (y.data[i] < 0) {
+                    y.data[i] = LRELU_COEFFICIENT * y.data[i];
+                }
+            }
+            break; 
+        case SOFTMAX:
+            for (int i = 0; i < y.rows; i++) {
+                float sum = 0;
+                for (int j = 0; j < y.cols; j++) {
+                    sum += exp(y.data[i*y.cols + j]);
+                }
+                for (int j = 0; j < y.cols; j++) {
+                    y.data[i*y.cols + j] = exp(y.data[i*y.cols + j]) / sum;
+                }
+            }
+            break;
+    }
 
     return y;
 }
@@ -47,6 +86,31 @@ matrix backward_activation_layer(layer l, matrix dy)
     // d/dx relu(x)     = 1 if x > 0 else 0
     // d/dx lrelu(x)    = 1 if x > 0 else 0.01
     // d/dx softmax(x)  = 1
+    switch(l.activation) {
+        case LINEAR:
+            break;
+        case LOGISTIC:
+            for (int i = 0; i < dx.rows * dx.cols; i++) {
+                dx.data[i] = logistic(x.data[i]) * (1 - logistic(x.data[i])) * dx.data[i];
+            }
+            break;
+        case RELU:
+            for (int i = 0; i < dx.rows * dx.cols; i++) {
+                if (x.data[i] < 0) {
+                    dx.data[i] = 0;
+                }
+            }
+            break;
+        case LRELU:
+            for (int i = 0; i < dx.rows * dx.cols; i++) {
+                if (x.data[i] < 0) {
+                    dx.data[i] = LRELU_COEFFICIENT * dx.data[i];
+                }
+            }
+            break; 
+        case SOFTMAX:
+            break;
+    }
 
     return dx;
 }
